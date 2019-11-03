4dc58960833803625862bdcf3da0536facd426aa
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-11-03 11:30:05 -0800

    Updates

---
+++ precache-manifest.02711e854dd3a27e88d2ca7bc33182a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6912b0c651d0e501bce7c5308e2522b",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "8b47d0e5487f66dd7c7b",
+    "url": "/react-memory-game/static/css/main.a2ea252c.chunk.css"
+  },
+  {
+    "revision": "4e774e2022bf4956a2e4",
+    "url": "/react-memory-game/static/js/2.63efbf31.chunk.js"
+  },
+  {
+    "revision": "8b47d0e5487f66dd7c7b",
+    "url": "/react-memory-game/static/js/main.dff8ca8e.chunk.js"
+  },
+  {
+    "revision": "fe346b25e1fa95144407",
+    "url": "/react-memory-game/static/js/runtime-main.3ac7a856.js"
+  }
+]);
